23398ad58c76f91b261c98ea0370b3666463d1aa
mp4253 <111766615+mp4253@users.noreply.github.com>
2023-07-10 19:45:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h2>This is a First php Program</h2>
+
+    <?php
+      echo "Hello World";
+    ?>
+
+</body>
+</html>
+
